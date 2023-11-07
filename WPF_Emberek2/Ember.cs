72f5e99dba0e35892bdf6dd4b2678aa849478b7b
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-07 17:19:35 +0100

    Add project files.

---
+++ WPF_Emberek2/Ember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Emberek2
+{
+    internal class Ember
+    {
+        private string name;
+        private int age;
+
+        public Ember(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
---
+++ WPF_Emberek2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Emberek2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Ember> list = new List<Ember>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            list.Add(new Ember("Gipsz Jakab", 42));
+            list.Add(new Ember("Lakatos Béla", 17));
+            list.Add(new Ember("Kovács Ferenc", 58));
+
+            emberek.ItemsSource = list;
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int age = 0;
+            if (string.IsNullOrWhiteSpace(textBoxName.Text) || string.IsNullOrWhiteSpace(textBoxAge.Text))
+            {
+                MessageBox.Show("Töltse ki mindkét mezőt!");
+            }
+            else
+            {
+                try
+                {
+                    age = int.Parse(textBoxAge.Text);
+                    if (age < 0 || age > 200)
+                    {
+                        MessageBox.Show("Helytelen életkor! (0-200 között)");
+                    }
+                    else
+                    {
+                        list.Add(new Ember(textBoxName.Text, age));
+                        emberek.Items.Refresh();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Helytelen formátumú kor!");
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Ember selected = emberek.SelectedItem as Ember;
+
+            if (selected == null)
+            {
+                MessageBox.Show("Válasszon ki egy törlendő adatsort!");
+            }
+            else
+            {
+                list.Remove(selected);
+                emberek.Items.Refresh();
+            }
+        }
+    }
+}
